7fdd98418b1200c730f6018fc1502eb71e48daed
Mohamed Sugal <sugaal@gmail.com>
2023-02-02 20:36:43 -0600

    fix distorted text on hero page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,21 +1,15 @@
-import { Container, Row } from 'react-bootstrap';
-import './styles.css';
+import { Container, Row } from "react-bootstrap";
+import "./styles.css";
 
 export default function Hero() {
   return (
-    <div className='cta'>
+    <div className="cta">
       <Container>
-        <Row className='col-lg-8'>
+        <Row className="col-lg-8">
           <h1>Find the best college the fits you and your learning style</h1>
           <p>Take control of your education right now!</p>
         </Row>
       </Container>
-      <div className='edu-logo float-right'>
-        <img
-          src='https://university-finder.s3.amazonaws.com/us_department_of_education.png'
-          alt='us-department-of-education'
-        />
-      </div>
     </div>
   );
 }
